6fd18d7976a523032b6a0373c943d63a2dd62ed8
raghuveersamarth <raghuveersamarth0@gmail.com>
2025-03-28 19:51:09 +0530

    database

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,12 +1,40 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
 
 export const authOptions = {  // ✅ Define authOptions
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    })
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "you@example.com" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Missing email or password");
+        }
+
+        // Check if user exists
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user) {
+          throw new Error("User not found");
+        }
+
+        return { id: user.id, name: user.name, email: user.email };
+      },
+    }),
   ],
   secret: process.env.NEXTAUTH_SECRET
 }
--- app/signup/page.js
+++ app/signup/page.js
@@ -1,119 +1,136 @@
-"use client"
-import {React, useState} from "react";
-import { useSession, signIn, signOut } from "next-auth/react"
+"use client";
+import { React, useState } from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const SignUpPage = () => {
   const { data: session } = useSession();
-  const handlesubmit = async (e) => {
-    let ndata = Object.fromEntries(e);
-    console.log(ndata);
-    const response = await fetch("/api/sign",{
+  const [form, setform] = useState({
+    name: "",
+    id: "",
+    email: "",
+    password: "",
+    role: "",
+  });
+  const handleChange = (e) => {
+    setform({ ...form, [e.target.name]: e.target.value });
+    console.log(form);
+  };
+  const handlesubmit = async () => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+      name: form.name,
+      email: form.email,
+      password: form.password,
+    });
+
+    const requestOptions = {
       method: "POST",
-      body:JSON.stringify({name: ndata.name,email: ndata.email,password: ndata.password,}),
-      headers: {
-        "Content-Type": "application/json",
-      },
-      
-    })
-    const data = await response.json();
-    console.log(data.message)
-  }
-    return (
-      <div className="min-h-screen bg-gray-50 flex flex-col justify-center items-center p-4">
-        <div className="w-full max-w-md bg-white rounded-lg shadow p-6">
-          <div className="text-center">
-            <p className="mb-3 text-2xl font-semibold leading-5 text-slate-900">
-              Create your account
-            </p>
-            <p className="mt-2 text-sm leading-4 text-slate-600">
-              Join us to get started.
-            </p>
-          </div>
-  
-          <div className="mt-7 flex flex-col gap-2">
-  
-            <button onClick={()=>signIn("google")} className="inline-flex cursor-pointer h-10 w-full items-center justify-center gap-2 rounded border border-slate-300 bg-white p-2 text-sm font-medium text-black outline-none focus:ring-2 focus:ring-[#333] focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-60">
-              <img
-                src="https://www.svgrepo.com/show/475656/google-color.svg"
-                alt="Google"
-                className="h-[18px] w-[18px]"
-              />
-              Continue with Google
-            </button>
-          </div>
-  
-          <div className="flex w-full items-center gap-2 py-6 text-sm text-slate-600">
-            <div className="h-px w-full bg-slate-200"></div>
-            OR
-            <div className="h-px w-full bg-slate-200"></div>
-          </div>
-  
-          <form className="w-full" action={handlesubmit}>
-            <label htmlFor="name" className="sr-only">
-              Full Name
-            </label>
-            <input
-              name="name"
-              type="text"
-              autoComplete="name"
-              required
-              className="block w-full rounded-lg border border-gray-300 px-3 py-2 shadow-sm outline-none placeholder:text-gray-400 focus:ring-2 focus:ring-black focus:ring-offset-1"
-              placeholder="Full Name"
-            />
-  
-            <label htmlFor="email" className="sr-only">
-              Email address
-            </label>
-            <input
-              name="email"
-              type="email"
-              autoComplete="email"
-              required
-              className="mt-2 block w-full rounded-lg border border-gray-300 px-3 py-2 shadow-sm outline-none placeholder:text-gray-400 focus:ring-2 focus:ring-black focus:ring-offset-1"
-              placeholder="Email Address"
-            />
-  
-            <label htmlFor="password" className="sr-only">
-              Password
-            </label>
-            <input
-              name="password"
-              type="password"
-              autoComplete="new-password"
-              required
-              className="mt-2 block w-full rounded-lg border border-gray-300 px-3 py-2 shadow-sm outline-none placeholder:text-gray-400 focus:ring-2 focus:ring-black focus:ring-offset-1"
-              placeholder="Password"
-            />
-  
-            <label htmlFor="confirm-password" className="sr-only">
-              Confirm Password
-            </label>
-            <input
-              name="confirm-password"
-              type="password"
-              autoComplete="new-password"
-              required
-              className="mt-2 block w-full rounded-lg border border-gray-300 px-3 py-2 shadow-sm outline-none placeholder:text-gray-400 focus:ring-2 focus:ring-black focus:ring-offset-1"
-              placeholder="Confirm Password"
-            />
-  
-            <button
-              type="submit"
-              className="mt-4 inline-flex w-full items-center justify-center rounded-lg bg-black p-2 py-3 text-sm font-medium text-white outline-none focus:ring-2 focus:ring-black focus:ring-offset-1 disabled:bg-gray-400"
-            >
-              Sign Up
-            </button>
-          </form>
-  
-          <div className="mt-6 text-center text-sm text-slate-600">
-            Already have an account?
-            <a href="/login" className="font-medium text-[#4285f4]">
-              Log in
-            </a>
-          </div>
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch("http://localhost:3000/api/sign", requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.error(error));
+
+    // Automatically log in after signing up
+    const loginResponse = await signIn("credentials", {
+      redirect: false,
+      email: form.email,
+      password: form.password,
+    });
+
+    if (loginResponse?.error) {
+      throw new Error(loginResponse.error);
+    }
+  };
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col justify-center items-center p-4">
+      <div className="w-full max-w-md bg-white rounded-lg shadow p-6">
+        <div className="text-center">
+          <p className="mb-3 text-2xl font-semibold leading-5 text-slate-900">
+            Create your account
+          </p>
+          <p className="mt-2 text-sm leading-4 text-slate-600">
+            Join us to get started.
+          </p>
+        </div>
+
+        <div className="flex w-full items-center gap-2 py-6 text-sm text-slate-600"></div>
+
+        <form className="w-full" action={handlesubmit}>
+          <label htmlFor="name" className="sr-only">
+            Full Name
+          </label>
+          <input
+            name="name"
+            type="text"
+            autoComplete="name"
+            onChange={handleChange}
+            required
+            className="block w-full rounded-lg border border-gray-300 px-3 py-2 shadow-sm outline-none placeholder:text-gray-400 focus:ring-2 focus:ring-black focus:ring-offset-1"
+            placeholder="Full Name"
+          />
+
+          <label htmlFor="email" className="sr-only">
+            Email address
+          </label>
+          <input
+            name="email"
+            type="email"
+            autoComplete="email"
+            onChange={handleChange}
+            required
+            className="mt-2 block w-full rounded-lg border border-gray-300 px-3 py-2 shadow-sm outline-none placeholder:text-gray-400 focus:ring-2 focus:ring-black focus:ring-offset-1"
+            placeholder="Email Address"
+          />
+
+          <label htmlFor="password" className="sr-only">
+            Password
+          </label>
+          <input
+            name="password"
+            type="password"
+            autoComplete="new-password"
+            onChange={handleChange}
+            required
+            className="mt-2 block w-full rounded-lg border border-gray-300 px-3 py-2 shadow-sm outline-none placeholder:text-gray-400 focus:ring-2 focus:ring-black focus:ring-offset-1"
+            placeholder="Password"
+          />
+
+          <label htmlFor="confirm-password" className="sr-only">
+            Confirm Password
+          </label>
+          <input
+            name="confirm-password"
+            type="password"
+            autoComplete="new-password"
+            required
+            className="mt-2 block w-full rounded-lg border border-gray-300 px-3 py-2 shadow-sm outline-none placeholder:text-gray-400 focus:ring-2 focus:ring-black focus:ring-offset-1"
+            placeholder="Confirm Password"
+          />
+          {session ? <p>logged in at {session.user.name}</p> : ""}
+          <button
+            type="submit"
+            className="mt-4 inline-flex w-full cursor-pointer items-center justify-center rounded-lg bg-black p-2 py-3 text-sm font-medium text-white outline-none focus:ring-2 focus:ring-black focus:ring-offset-1 disabled:bg-gray-400"
+          >
+            Sign Up
+          </button>
+        </form>
+
+        <div className="mt-6 text-center text-sm text-slate-600">
+          Already have an account?
+          <a href="/login" className="font-medium text-[#4285f4]">
+            Log in
+          </a>
         </div>
       </div>
-    );
-  };
-  
-  export default SignUpPage;
+    </div>
+  );
+};
+
+export default SignUpPage;
