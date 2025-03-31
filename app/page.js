5ecad983de910a3165c34eaff3bf1af9f96abdfa
raghuveersamarth <raghuveersamarth0@gmail.com>
2025-03-31 13:34:25 +0530

    tools

--- app/page.js
+++ app/page.js
@@ -2,7 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="m-auto w-3/4 h-screen flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-bold mb-4">Tools</h1> 
     {/* huge description of Tools master */}
     </div>
   );
