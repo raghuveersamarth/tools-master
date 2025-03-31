722d2d54af6bd5daa95e1392fff144fcf597ca78
Chinmay <chinmaydhondale@gmail.com>
2025-03-31 14:53:17 +0530

    adding username

--- app/api/sign/route.js
+++ app/api/sign/route.js
@@ -4,21 +4,30 @@ import { NextResponse } from "next/server"
 const handler = async function(req)  {
     if(req.method === "POST"){
         
-        const {name, email, password} = await req.json()
+        const {username, password} = await req.json()
         try{
             
             const existingUser = await prisma.user.findUnique({
-                where: {email},
+                where: {username},
             });
            
             if (existingUser){
                 return Response.json({message: "Log in"})
             }
             else{
+                let newId = 1;
+                for (let i = 0; i < users.length; i++) {
+                  if (users[i].id !== i + 1) {
+                    newId = i + 1;
+                    break;
+                  }
+                }
+                if (newId === 1 && users.length > 0) newId = users.length + 1;
+
                 const newUser = await prisma.user.create({
-                    data:{name, email, password},
+                    data:{id:newId,password:password,username:username},
                 })
-                return Response.json({message: `User ${name} created`})
+                return Response.json({message: `User ${username} created`})
             }
         }
         catch(error){
